fe071fe837ab4274374ea85c78c560376575646d
Sonny <Sonnyhaack@gmail.com>
2021-03-23 14:34:46 +0100

    Client

--- src/Client.java
+++ src/Client.java
@@ -1,8 +1,13 @@
 import Calc.CalculatorVisitor;
 import Operand.Token;
 
+import java.util.ArrayList;
+
 public class Client
 {
  private CalculatorVisitor calculatorVisitor;
  private Token token;
+
+ public int evaluateExpression(ArrayList<Token>){}
+
 }
